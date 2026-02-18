8bb450037686b630555749d6702ad5190e0cbb19
marco <marco@MARCO-PC>
2026-02-18 02:22:44 -0600

    config file changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio/'   // <-- MUST match the GitHub repo name
 })
